040bc29047ea6bb6fe89aa34162eb654fbe5cb5c
jiro.kirameki <ec2-user@ip-172-31-22-207.us-east-2.compute.internal>
2018-06-18 03:41:50 +0000

    want

--- app/Http/Controllers/ItemsController.php
+++ app/Http/Controllers/ItemsController.php
@@ -33,4 +33,16 @@
             'items' => $items,
         ]);
     }
+    
+    public function show($id)
+    {
+      $item = Item::find($id);
+      $want_users = $item->want_users;
+
+      return view('items.show', [
+          'item' => $item,
+          'want_users' => $want_users,
+      ]);
+    }
+    
   }
